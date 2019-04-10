401ec5bca49f371285188cee01e62453c3de8177
jubaer-hossain <jubaer1660@gmail.com>
2019-04-10 08:21:56 +0600

    Gridview

---
+++ test/admin/home.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="test.admin.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+
+    <form id="form2" runat="server">
+
+        <div>
+            <label>Username</label>
+            <asp:TextBox ID="username" runat="server"></asp:TextBox>
+            <label>Name</label>
+            <asp:TextBox ID="name" runat="server"></asp:TextBox>
+            <asp:Button ID="btn" runat="server" Text="Submit" OnClick="btn_Click"/>
+        </div>
+
+    <asp:GridView ID="grid1" runat="server" AutoGenerateColumns="False" CellPadding="4" ForeColor="#333333" GridLines="None" >  
+     <AlternatingRowStyle BackColor="White" />  
+     <columns>  
+         <asp:TemplateField HeaderText="Admin Name">  
+             <ItemTemplate>  
+                 <asp:Label ID="LblCompanyId" runat="server" Text='<%#Bind("username") %>'></asp:Label>  
+             </ItemTemplate>  
+         </asp:TemplateField>  
+         <asp:TemplateField HeaderText="Name">  
+             <ItemTemplate>  
+                 <asp:Label ID="LblCompanyName" runat="server" Text='<%#Bind("name") %>'></asp:Label>  
+             </ItemTemplate>  
+         </asp:TemplateField>  
+          
+     </columns>  
+      
+ </asp:GridView> 
+        </form>
+</body>
+</html>
---
+++ test/admin/home.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace test.admin
+{
+    public partial class home : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\jubaer\source\repos\test\test\App_Data\admin_main.mdf;Integrated Security = True");
+        protected void Page_Load(object sender, EventArgs e)
+        {   
+            
+        }
+     
+        public DataTable display_records()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("select * from admin_info", con);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            grid1.DataSource = dt;
+            grid1.DataBind();
+
+            return dt;
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+
+            insert_data(username.Text, name.Text);
+            display_records();
+        }
+
+        protected void insert_data(string username, string name)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into admin_info values ('" + username+ "', '" + name + "')";
+            cmd.ExecuteNonQuery();
+
+
+        }
+    }
+}
